fc6da2d83a6458826b4e0dbae5cb8b974e35cb16
sptls <sp0tlesss@yahoo.com>
2022-09-04 16:42:49 +0000

    cleaning

--- src/consts.h
+++ src/consts.h
@@ -5,11 +5,8 @@
 
 #define SYMBOL_MAX_SPEED 15
 #define SYMBOL_MAX_SIZE 4
-#define SYMBOL_MAX_LEN 20
+#define SYMBOL_MAX_LEN 25
 #define NR_OF_SYMBOLS 74
-
-#define WIN_H 1440
-#define WIN_W 2000
 
 #define END_SCREEN_W 510
 #define END_SCREEN_H 260
